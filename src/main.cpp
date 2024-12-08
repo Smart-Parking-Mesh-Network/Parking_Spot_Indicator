a3af5531b56a12ac67f924e3bb686a6ee3795fbe
adel4780 <adelk138019@gmail.com>
2024-12-08 10:11:56 +0330

    Add elevator Score & Elevator Score Button

--- src/main.cpp
+++ src/main.cpp
@@ -3,21 +3,22 @@
 #include <SoftwareSerial.h>
 
 // Defines
-#define BUTTON_PIN 2         
+#define ENTRANCE_SCORE_BUTTON 2
+#define ELEVATOR_SCORE_BUTTON 5         
 #define DEBOUNCE_DELAY 150
 #define TRIGGER_PIN 6
 #define MAX_SECTIONS 4
 #define UART_TIMEOUT 2000
 #define RX_PIN 3
 #define TX_PIN 4             
-#define MAX_RESERVATIONS 10
+#define MAX_RESERVATIONS 20
 #define RESERVATION_TIMEOUT 30000
 #define SHOW_RESULT 3000
 
 // Variables
-unsigned long lastDebounceTime = 0;
-int lastButtonState = HIGH;
-int buttonState;
+unsigned long lastEntranceDebounceTime  = 0, lastElevatorDebounceTime  = 0;
+int lastEntranceButtonState = HIGH, lastElevatorButtonState = HIGH;
+int entranceButtonState, elevatorButtonState;
 SoftwareSerial softSerial(RX_PIN, TX_PIN);
 
 // LCD pin configuration
@@ -28,6 +29,7 @@ struct ParkingSection {
   char section[16];
   int spots;
   int entranceScore;
+  int elevatorScore;
   int score;
 };
 ParkingSection parkingList[MAX_SECTIONS];
@@ -38,10 +40,9 @@ struct Reservation {
 };
 Reservation reservationList[MAX_RESERVATIONS];
 int parkingCount = 0;
-int reservationCount = 0;                      
-int currentDisplayIndex = 0;              
+int reservationCount = 0;             
 bool flag = false; 
-int selectiveScore = 0; // 1: sort by entranceScore
+int selectiveScore = 0; // 1: sort by entranceScore, 2: sort by elevatorScore
 
 // Function declarations
 void checkButton();
@@ -56,7 +57,8 @@ int getReservedCount(const char* sectionName);
 void setup() {
   Serial.begin(9600);
   softSerial.begin(9600);
-  pinMode(BUTTON_PIN, INPUT_PULLUP); 
+  pinMode(ENTRANCE_SCORE_BUTTON, INPUT_PULLUP);
+  pinMode(ELEVATOR_SCORE_BUTTON, INPUT_PULLUP); 
   pinMode(TRIGGER_PIN, OUTPUT);        
   digitalWrite(TRIGGER_PIN, HIGH);
   lcd.begin(16, 2);                    
@@ -70,23 +72,42 @@ void loop() {
 
 // Function definitions
 void checkButton() {
-  int currentButtonState = digitalRead(BUTTON_PIN); 
-  if (currentButtonState != lastButtonState) {
-    lastDebounceTime = millis(); // Update last change time
+  int currentEntranceButtonState  = digitalRead(ENTRANCE_SCORE_BUTTON); 
+  int currentElevatorButtonState  = digitalRead(ELEVATOR_SCORE_BUTTON); 
+ 
+  if (currentEntranceButtonState  != lastEntranceButtonState) {
+    lastEntranceDebounceTime = millis(); // Update last change time
+  }
+  if (currentElevatorButtonState  != lastElevatorButtonState) {
+    lastEntranceDebounceTime = millis(); // Update last change time
   }
 
-  if ((millis() - lastDebounceTime) > DEBOUNCE_DELAY) {
-    if ( currentButtonState != buttonState ) {
-      buttonState = currentButtonState;
-      if (buttonState == LOW) { // Button is pressed
+  if ((millis() - lastEntranceDebounceTime) > DEBOUNCE_DELAY) {
+    if (currentEntranceButtonState != entranceButtonState) {
+      entranceButtonState = currentEntranceButtonState ;
+      if (entranceButtonState == LOW) { // Button is pressed
         selectiveScore = 1;
         lcd.clear();
-        lcd.print("Wait...");
+        lcd.print("Entrance Wait...");
         clearParkingList();
         fetchDataFromNetwork();
       }
       }
     }
+  lastEntranceButtonState = currentEntranceButtonState;
+
+  if ((millis() - lastElevatorDebounceTime) > DEBOUNCE_DELAY) {
+  if (currentElevatorButtonState != elevatorButtonState) {
+    elevatorButtonState = currentElevatorButtonState ;
+    if (elevatorButtonState == LOW) { // Button is pressed
+      selectiveScore = 2;
+      lcd.clear();
+      lcd.print("Elevator Wait...");
+      clearParkingList();
+      fetchDataFromNetwork();
+    }
+    }
+  }
   if (flag) {
     Serial.println("Updating Display");
     quickSort(parkingList, 0, parkingCount - 1);
@@ -94,7 +115,8 @@ void checkButton() {
     displayParking();
     flag = false;
   }
-  lastButtonState = currentButtonState; 
+  lastElevatorButtonState = currentElevatorButtonState;
+
 }
 
 // Function to fetch data from ESP
@@ -119,10 +141,13 @@ void fetchDataFromNetwork() {
       if (strcmp(buffer, "END") > 0) break;
 
       // store the received data : A 10 2
-      sscanf(buffer, "%s %d %d", parkingList[parkingCount].section, &parkingList[parkingCount].spots, &parkingList[parkingCount].entranceScore);
+      sscanf(buffer, "%s %d %d %d", parkingList[parkingCount].section, &parkingList[parkingCount].spots, &parkingList[parkingCount].entranceScore, &parkingList[parkingCount].elevatorScore);
       // store the selective score 
       if(selectiveScore == 1) {
         parkingList[parkingCount].score = parkingList[parkingCount].entranceScore;
+      }
+      if (selectiveScore == 2) {
+        parkingList[parkingCount].score = parkingList[parkingCount].elevatorScore;
       }
       parkingCount++;
       Serial.println("Data Added");
@@ -153,7 +178,7 @@ void displayParking() {
       lcd.print("Sec: ");
       lcd.print(parkingList[i].section);
       lcd.print(" Score: ");
-      lcd.print(parkingList[currentDisplayIndex].score);
+      lcd.print(parkingList[i].score);
       lcd.setCursor(0, 1);
       lcd.print("Spot: ");
       lcd.print(remainingSpots);
@@ -177,10 +202,10 @@ void clearParkingList() {
     memset(parkingList[i].section, 0, sizeof(parkingList[i].section)); // Clear section name
     parkingList[i].spots = 0;         // Reset spots
     parkingList[i].entranceScore = 0; // Reset entrance score
+    parkingList[i].elevatorScore = 0; // Reset elevator score
     parkingList[i].score = 0;
   }
   parkingCount = 0;           // Reset parking count
-  currentDisplayIndex = 0 ;
 }
 
 // Swap function to swap two ParkingSection elements
